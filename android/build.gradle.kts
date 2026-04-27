c4042050503b97f4c33650e453c7316e17e35b28
User <user@example.com>
2026-04-27 15:37:40 +0300

    fix(android): properly resolve different roots error by limiting build dir redirection

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,17 +12,15 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    if (project.projectDir.path.startsWith(rootProject.projectDir.path)) {
+        val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+        project.layout.buildDirectory.value(newSubprojectBuildDir)
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
-    tasks.whenTaskAdded {
-        if (name.contains("UnitTest", ignoreCase = true)) {
-            enabled = false
-        }
-    }
 }
+
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)

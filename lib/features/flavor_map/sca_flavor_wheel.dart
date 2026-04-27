dcd739fbde601c096a48cecb1f24048bd93d4b18
User <user@example.com>
2026-04-27 17:17:41 +0300

    style: remove unused dart:ui import

--- lib/features/flavor_map/sca_flavor_wheel.dart
+++ lib/features/flavor_map/sca_flavor_wheel.dart
@@ -1,5 +1,4 @@
 import 'dart:math' as math;
-import 'dart:ui' show ImageFilter;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
